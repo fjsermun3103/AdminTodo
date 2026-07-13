68cf237da18deefc6bc5199e0b4b507cf27c22af
Francisco José Serrano Muñoz <franciscojoseserranomunoz@gmail.com>
2026-07-13 20:07:43 +0200

    Restful API handlers

---
+++ app/api/todos/[id]/route.ts
@@ -0,0 +1,58 @@
+import { Todo } from '@/app/generated/prisma/client';
+import prisma from '@/app/lib/prisma';
+import { NextResponse, NextRequest } from 'next/server'
+import * as yup from 'yup'
+
+interface Segments {
+  params: {
+    id: string;
+  }
+}
+
+const getTodo = async(id: string): Promise<Todo | null> => {
+  const todo = await prisma.todo.findFirst({ where: { id } });
+
+  return todo;
+};
+
+
+export async function GET(request: Request, { params }: Segments) {
+  const { id } = await params;
+
+  const todo = await getTodo(id);
+
+  if (!todo) {
+    return NextResponse.json({ message: `El ID ${id} no ha sido encontrado` }, { status: 404 });
+  };
+
+  return NextResponse.json(todo);
+};
+
+const putSchema = yup.object({
+  complete: yup.boolean().optional(),
+  description: yup.string().optional(),
+});
+
+
+export async function PUT(request: Request, { params }: Segments) {
+  const { id } = await params;
+
+  const todo = await getTodo(id);
+
+  if (!todo) {
+    return NextResponse.json({ message: `El ID ${id} no ha sido encontrado` }, { status: 404 });
+  };
+
+  try {
+    const { complete, description } = await putSchema.validate(await request.json());
+
+    const updatedTodo = await prisma.todo.update({
+      where: { id },
+      data: { complete, description }
+    });
+
+    return NextResponse.json(updatedTodo);
+  } catch (error) {
+    return NextResponse.json(error, { status: 400 });
+  };
+};
