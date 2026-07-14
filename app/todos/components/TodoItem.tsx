c7222b2ac0a8e7a289760c6ede94aa653fed5651
Francisco José Serrano Muñoz <franciscojoseserranomunoz@gmail.com>
2026-07-14 14:23:49 +0200

    Actualizar Todo

--- app/todos/components/TodoItem.tsx
+++ app/todos/components/TodoItem.tsx
@@ -1,21 +1,26 @@
+'use client'
+
 import { IoCheckboxOutline, IoSquareOutline } from 'react-icons/io5';
 import { Todo } from '../interfaces/todo';
 
 import styles from './TodoItem.module.css';
 interface Props {
     todo: Todo;
-    //TODO: Acciones que quiero llamar
+    toggleTodo: (id: string, complete: boolean) => Promise<Todo | void>;
 }
 
-export const TodoItem = ({ todo }: Props) => {
+export const TodoItem = ({ todo, toggleTodo }: Props) => {
     return (
         <div className={todo.complete ? styles.todoDone : styles.todoPending}>
             <div className="flex flex-col sm:flex-row justify-start items-center gap-4">
-                <div className={`
-                    flex p-1 rounded-md cursor-pointer
-                    hover:bg-opacity-60
-                    ${todo.complete ? 'bg-blue-100' : 'bg-red-100'} 
-                `}>
+                <div
+                    onClick={() => toggleTodo(todo.id, !todo.complete)}
+                    className={`
+                        flex p-1 rounded-md cursor-pointer
+                        hover:bg-opacity-60
+                        ${todo.complete ? 'bg-blue-100' : 'bg-red-100'} 
+                    `}
+                >
                     {
                         todo.complete
                             ? <IoCheckboxOutline size={25} />
